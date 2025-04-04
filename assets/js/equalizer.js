abb40b0c97ecfe73a5ff1a4984e87e9496589876
m-delamore <cosmicmuwanderer@gmail.com>
2025-04-04 16:19:53 +0800

    Changed background for hero page..

---
+++ assets/js/equalizer.js
@@ -0,0 +1,8 @@
+/* const bars = document.querySelectorAll(".bar");
+
+setInterval(() => {
+    bars.forEach((bar, index) => {
+        let randomHeight = Math.floor(Math.random() * 120) + 30; // Varying heights
+        bar.style.height = randomHeight + "px";
+    });
+}, 200); */
